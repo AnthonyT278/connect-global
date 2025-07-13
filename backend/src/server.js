95eb4aca590cf98cb2baa8baaf6edff66d97b066
AnthonyT278 <tmoropodi1@gmail.com>
2025-07-13 13:00:22 +0200

    startServer func added

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,4 +8,18 @@ connectDB();
 
 app.get("/", (req, res) => res.send("Welcome to the Connect Global API"));
 
-app.listen(ENV.PORT, () => console.log("Server is running on port",  ENV.PORT));
+// app.listen(ENV.PORT, () => console.log("Server is running on port",  ENV.PORT));
+
+const startServer  = async () => {
+    try {
+        await connectDB();
+
+        app.listen(ENV.PORT, () => console.log("Server is up and running on Port:", ENV.PORT));
+    } catch (error) {
+      console.error("Failed to start server: ", error.massage);
+      process.exit(1);
+    }
+};
+
+startServer();
+
